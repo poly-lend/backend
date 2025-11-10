0100032062263b9df997d693e4985456f0218b4a
polylend-dev <polylend@tuta.com>
2025-11-10 06:56:51 +0400

    add express

--- src/runner.ts
+++ src/runner.ts
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv'
+import express from 'express'
 import logger from './utils/logger'
 dotenv.config()
 
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 async function main() {
   logger.info('🚀 Starting...\n')
-
+  app.listen(port, () => {
+    logger.info(`Listening on port ${port}`)
+  })
 }
 
 main().catch(console.error)
