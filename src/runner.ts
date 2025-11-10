d9278f0c9aea0761186a2a355ac7a28f6546714b
polylend-dev <polylend@tuta.com>
2025-11-10 07:41:40 +0400

    polymarket api proxy

--- src/runner.ts
+++ src/runner.ts
@@ -6,12 +6,28 @@ dotenv.config()
 const app = express()
 const port = 3001
 
-app.get('/markets', (req, res) => {
-  res.send('Hello World')
+app.get('/markets', async (req, res) => {
+  let clobTokenIds = req.query.clob_token_ids
+  if (!clobTokenIds) {
+    res.status(400).send('clob_token_ids is required')
+    return
+  }
+
+  const params = (Array.isArray(clobTokenIds) ? clobTokenIds : [clobTokenIds])
+    .map((param) => param.toString())
+    .map((id: string) => `clob_token_ids=${id}`)
+  const url = `http://gamma-api.polymarket.com/markets?${params.join('&')}`
+  const response = await fetch(url)
+  if (!response.ok) {
+    res.status(response.status).send(response.statusText)
+    return
+  }
+  const data = await response.json()
+  res.send(data)
 })
 
 async function main() {
-  logger.info('🚀 Starting...\n')
+  logger.info('🚀 Starting...')
   app.listen(port, () => {
     logger.info(`Listening on port ${port}`)
   })
