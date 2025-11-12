aa6ac3491dd6244afa4b7b5033780a7a61cb7641
polylend-dev <polylend@tuta.com>
2025-11-12 13:30:26 +0400

    add events

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const RPC_URL = process.env.RPC_URL
 export const WS_URL = process.env.WS_URL
-export const polylendAddress = '0x7bCaA23aB1777C7c19935E3872A165B10cD0F650'
+export const polylendAddress = '0x800703411c24EC90eC4B2647624C39ef315570F6'
 export const START_BLOCK = process.env.START_BLOCK ? Number(process.env.START_BLOCK) : 78876000
 export const BLOCK_INTERVAL = 10000
 export const MONGO_URL = process.env.MONGO_URL!
