e17c6bd0795f3635a71ec9f7a87cc0bd99225ada
polylend-dev <polylend@tuta.com>
2025-11-26 11:01:29 +0400

    switch to new contract

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const RPC_URL = process.env.RPC_URL
 export const WS_URL = process.env.WS_URL
-export const polylendAddress = '0x9BF793E4759154D84313485ca8b95fB933Aac1d6'
+export const polylendAddress = '0x75cffbed1cdb40ec0ba2164e213950727164fdb0'
 export const START_BLOCK = process.env.START_BLOCK ? Number(process.env.START_BLOCK) : 78876000
 export const BLOCK_INTERVAL = 10000
 export const MONGO_URL = process.env.MONGO_URL!
