ea5886b0925299bd3a1828f343a6660a3da67ddf
polylend-dev <polylend@tuta.com>
2025-12-01 08:21:15 +0400

    support multiple collateral amounts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const RPC_URL = process.env.RPC_URL
 export const WS_URL = process.env.WS_URL
-export const polylendAddress = '0x683216767FF2dd123406C69B724258c37Ab6F949'
+export const polylendAddress = '0x092b68bb8b1da1639237593dcb60f00c7a276f45'
 export const START_BLOCK = process.env.START_BLOCK ? Number(process.env.START_BLOCK) : 79516000
 export const BLOCK_INTERVAL = 10000
 export const MONGO_URL = process.env.MONGO_URL!
