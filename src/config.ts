bf41c49457536d64f5567793d1c230b6b2d5c97b
polylend-dev <polylend@tuta.com>
2026-03-27 18:50:02 -0700

    update block

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ dotenv.config()
 export const RPC_URL = process.env.RPC_URL
 export const WS_URL = process.env.WS_URL
 export const polylendAddress = '0x1620A7d943B0DeAf1c2123BE6413F87B5dacEf2b'
-export const START_BLOCK = process.env.START_BLOCK ? Number(process.env.START_BLOCK) : 79726000
+export const START_BLOCK = process.env.START_BLOCK ? Number(process.env.START_BLOCK) : 84700000
 export const BLOCK_INTERVAL = 10000
 export const MONGO_URL = process.env.MONGO_URL!
